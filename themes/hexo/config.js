ee2372b79ba38c4fafde1cd99b61c2868fd19eeb
chacat68 <cainefox@gmail.com>
2024-10-17 11:06:50 +0800

    更换主题

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -3,12 +3,11 @@ const CONFIG = {
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HEXO_HOME_BANNER_GREETINGS: [
     '爱好美食',
-    '热爱音乐',
+    '热爱游戏',
     '热爱编程', 
     '热爱写作',
     '创作达人',
     '热爱生活',
-    '喜欢分享'
   ], // 首页大图标语文字
 
   HEXO_HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
