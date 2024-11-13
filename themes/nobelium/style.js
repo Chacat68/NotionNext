28da213699779c518d6e42731db153c9014feb54
chacat68 <cainefox@gmail.com>
2024-11-13 21:21:11 +0800

    更新nobelium主题样式，调整目录组件定位方式为绝对定位并修改外边距以优化布局

--- themes/nobelium/style.js
+++ themes/nobelium/style.js
@@ -22,10 +22,9 @@ const Style = () => {
 
     /* 目录组件样式 */
     .catalog-container {
-      position: relative; /* 相对定位，使元素相对于其正常位置进行定位 */
-      top: 50%; /* 将元素从顶部向下移动50% */
-      left: 50%; /* 将元素从左侧向右移动50% */
-      transform: translate(-50%, -50%); /* 使用transform将元素的中心点移动到屏幕的中间 */
+      position: absolute;
+      left: 0;
+      top: 0;
       margin-left: 120rem; /* 左外边距为 20rem，用于控制目录与文章的距离 */
       max-width: 15rem; /* 最大宽度为 12rem，防止目录过宽 */
     }
